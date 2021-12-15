d2ee149281cf4ac9fecbf3e825320b05ae77e470
arjunps96 <arjun.sadanandan@qburst.com>
2021-12-15 16:21:54 +0530

    added color changing logic

--- src/component/MatrixComponent.js
+++ src/component/MatrixComponent.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/matrix.min.css";
 
 const MatrixComponent = () => {
   const matrixArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-  let colorChangeClass;
+  let colorChangeClass = "bg-red";
 
-  const changeColorToRed = (item) => {
-    colorChangeClass = `.item-${item}`;
+  const [className, setclassName] = useState("");
+
+  let contents = matrixArray.map((item, index) => (
+    <div
+      className={className || "item"}
+      key={index}
+      data-element={item}
+      onClick={(e) => changeColorToRed(e)}
+    ></div>
+  ));
+  const changeColorToRed = (e) => {
+    const id = e.target.getAttribute("data-element");
+
+    setclassName(`item item-${id}`);
   };
 
   return (
     <>
-      <div className="grid">
-        {matrixArray.map((item, index) => (
-          <div
-            className={colorChangeClass || "item"}
-            key={index}
-            id={index}
-            onClick={changeColorToRed.bind(this, item)}
-          ></div>
-        ))}
-      </div>
+      <div className="grid">{contents}</div>
     </>
   );
 };
